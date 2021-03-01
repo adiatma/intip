2b92ae53a99eedf58a06b4660328ae0ec9974229
adiatma <adiatma9024@gmail.com>
2021-03-02 00:30:41 +0800

    chore: mv progress_bar() to install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,21 @@ JQ_OSX=jq-osx-amd64
 JQ_LINUX=jq-linux64
 JQ_DOWNLOAD=https://github.com/stedolan/jq/releases/download
 
+progress_bar() {
+  local duration=${1}
+    already_done() { for ((done=0; done<$elapsed; done++)); do printf ":"; done }
+    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+    clean_line() { printf "\r"; }
+
+  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
+      already_done; remaining; percentage
+      clean_line
+  
+  done
+    clean_line
+}
+
 download_and_install_intip() {
   command curl -fsSL ${SOURCE} -o $HOME/intip.sh
   command mv $HOME/intip.sh $BIN_LOCATION
@@ -33,10 +48,14 @@ download_and_install_jq() {
 main() {
   if [[ ! -s "$JQ_BIN_LOCATION" ]]; then
     echo "🚀 download and install jq"
+    progress_bar 100
+    printf "\n"
     download_and_install_jq
   fi
 
   echo "🚀 download and install intip.sh"
+  progress_bar 100
+  printf "\n"
   download_and_install_intip
 }
 
--- intip.sh
+++ intip.sh
@@ -4,21 +4,6 @@ HOST=http://ip-api.com
 OUTPUT="json"
 FIELDS="?fields=66846719"
 
-progress_bar() {
-  local duration=${1}
-    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
-    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
-    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
-    clean_line() { printf "\r"; }
-
-  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
-      already_done; remaining; percentage
-      clean_line
-  
-  done
-    clean_line
-}
-
 print_help() {
   cat <<EOF
 
@@ -35,14 +20,12 @@ EOF
 prompt_read_ip() {
   echo -n "Input your ip in here?: "
   read IP_LOCATION
-  progress_bar 100
   URL="${HOST}/${OUTPUT}/${IP_LOCATION}${FIELDS}"
   GET_DATA=$(curl "$URL" --silent)
 }
 
 read_inline() {
   IP_LOCATION=${1}
-  progress_bar 100
   URL="${HOST}/${OUTPUT}/${IP_LOCATION}${FIELDS}"
   GET_DATA=$(curl "$URL" --silent)
 }
